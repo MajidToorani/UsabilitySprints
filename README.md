5dc4f4f13f0f738d515188f66a42e8b9c7349f3a
Majid Tooranisama <mtooranisama5070@conestogac.on.ca>
2019-02-12 22:19:45 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# UsabilitySprints
---
+++ UsabilitySprints/wwwroot/js/Sprint1.js
@@ -0,0 +1,36 @@
+﻿/*
+ * Assignment #1
+ * Majid Tooranisama , Created : Feb 5, 2019
+ */
+ function BackColor(color) {
+    var myText = document.getElementById("US1originalText").value;    
+    document.getElementById("greenBackground").value = myText;
+    document.getElementById("greenBackground").style.backgroundColor = color;
+};
+
+String.prototype.reverse = function () {
+    return this.split('').reverse().join('');
+}
+
+function ReverseMyText() {
+    var myText = document.getElementById("US5originalText").value;
+    var reversed = myText.reverse();
+    document.getElementById("reversedText").value = reversed;
+};
+
+function Bold() {
+    var myText = document.getElementById("US3originalText").value;
+    var bolded = myText.bold();
+    document.getElementById("boldText").innerHTML = bolded;
+};
+
+function Italicize() {
+    var myText = document.getElementById("US4originalText").value;
+    var italic = myText.italics();
+    document.getElementById("italicizedText").innerHTML = italic;
+};
+
+function ChangeBackground(color) {
+    document.body.style.backgroundColor = color;
+};
+
